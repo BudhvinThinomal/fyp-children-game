3a9fcdbf3d9166231d0c39e0d42f623697e04272
Budhvin Thinomal <61777208+BudhvinThinomal@users.noreply.github.com>
2023-03-27 02:34:55 +0530

    Nav bar changes added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import GameView from "./components/GameView";
-import Home from "./components/Home";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import GameView from "./pages/GameView";
 
 const theme = createTheme();
 
 function App() {
-  const [gameStarted, setGameStarted] = useState(false);
-
-  const handleStart = () => {
-    setGameStarted(true);
-  };
-
-  const handleReset = () => {
-    setGameStarted(false);
-  };
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      {gameStarted ? (
-        <GameView onReset={handleReset} />
-      ) : (
-        <Home open={true} onClose={() => {}} onStart={handleStart} />
-      )}
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/GameView" component={GameView} />
+          {/* <Route path="/products" component={Products} />
+          <Route path="/sign-up" component={SignUp} /> */}
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
